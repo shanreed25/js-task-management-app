b6adac7445d519ed5213a919b025be99e2db92fe
shanreed25 <shannontlreed@gmail.com>
2026-09-18 11:45:42 -0700

    updated the addtask function with more readable values

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let tasks = [];
+let counter = 0;
 
 function addTask(name, category, deadline, status ="Not Started"){
-    let newTask = {taskName: name, taskCategory: category, taskDeadline: deadline, taskStatus: status}
+    let newTask = {id: counter++, name: name, category: category, deadline: deadline, status: status}
     tasks.push(newTask)
 
 }
 
 addTask("Write report", "Work", "2026-09-25", "In Progress");
+addTask("Buy groceries", "Personal", "2026-09-19");
+addTask("Write report", "School", "2026-10-19", "In Progress");
 console.log(tasks)
+
+function updateStatus(taskId, newStatus){
+
+}
