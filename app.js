faeaef392ab7949935df4c5f98a56e49fe607f7c
shanreed25 <shannontlreed@gmail.com>
2026-09-21 21:19:45 -0700

    added a id for each task and a edit button for each

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let tasks = [];
 let taskNameInput = document.getElementById("task-name");
 let taskCategoryInput = document.getElementById("task-category");
 let taskDeadlineInput = document.getElementById("task-deadline");
@@ -9,9 +8,12 @@ let taskListSection = document.getElementById("task-list-section");
 let noTaskMessage = document.getElementById("no-task-message");
 // let statusDropdown = document.createElement("select")
 
-const STATUES = ["Not Started", "In Progress", "Paused", "Done"]
 
-let taskId = 1;
+let tasks = [];
+let taskId = 0;//to be able to acces a certain task i need an id
+
+//takes the task input information and add it to the task list
+// then clears the input field
 function addTask(name, category, deadline, status) {
   let newTask = {
     //create the task
@@ -26,31 +28,44 @@ function addTask(name, category, deadline, status) {
   clearFields();
 }
 
-function displayTask() {
-    noTaskMessage.hidden = tasks.length > 0;//Clears no tasks message when there are tasks
-    for (let task of tasks) {
-        //For every task create a Task Card
-        let taskItem = document.createElement("li"); //create a list element
-        let taskInfo = document.createElement("div"); //create a div element to hold task information
-        taskInfo.classList.add("task");
+//Create a task item list to be displayed in the UI
+function createTaskElements(){
+      for (let task of tasks) {
+        //
+        /*
+          For every task create a Task Card with this shape
+          <li><div><h3></h3><h5></h5><h5></h5><h5></h5></div><button></button></li>
+        */
+        let taskItem = document.createElement("li");
+        let taskInfo = document.createElement("div");
+        taskInfo.className = "task";
         let taskTitle = document.createElement("h3");
-        let taskCategory = document.createElement("h5");
-        let taskDeadline = document.createElement("h5");
-        let taskStatus = document.createElement("h5");
+        let taskCategory = document.createElement("p");
+        let taskDeadline = document.createElement("p");
+        let taskStatus = document.createElement("p");
+        let taskEditButton = document.createElement('button')
 
-        //Give the card the task content
+        //Give the card elements the content of the task
         taskTitle.innerText = task.name;
         taskCategory.innerText = `Category | ${task.category}`;
         taskDeadline.innerText = `Deadline | ${task.deadline}`;
         taskStatus.innerText = `Status | ${task.status}`;
+        taskEditButton.innerText = "EDIT";
         
 
         //Display Task in HTML
-        taskInfo.append(taskTitle, taskStatus, taskCategory, taskDeadline );
-        taskItem.appendChild(taskInfo);
+        taskInfo.append(taskTitle, taskStatus, taskCategory, taskDeadline, taskEditButton );
+        taskItem.append(taskInfo);
         taskList.appendChild(taskItem);
     }
 }
+
+function displayTask() {
+    noTaskMessage.hidden = tasks.length > 0;//Clears no tasks message when there are tasks
+    createTaskElements();
+}
+
+
 
 function clearFields() {
   taskNameInput.value = "";
@@ -63,11 +78,18 @@ function removeTaskList() {
   taskList.innerHTML = "";
 }
 
+function updateStatus(taskId, newStatus){
+    let task = tasks.find(t => t.id === taskId);
+    task.status = newStatus
+    return task;
 
+}
 
-//==================================================================================
 
 displayTask();
+//==================================================================================
+
+
 
 addTaskButton.addEventListener("click", function () {
   let taskName = taskNameInput.value; //get the input value
@@ -86,11 +108,6 @@ addTaskButton.addEventListener("click", function () {
   
 });
 
-statusDropdown.addEventListener("click", function (e){
-  // let newStatus = statusDropdown.value
-  // console.log(task);
-  console.log(statusDropdown.value)
-})
 
 
 //===================================================
--- test.js
+++ test.js
@@ -1,35 +1,44 @@
+
+
+
 const tasks = [
   {
+    id: 1,
     name: "Go shopping for a dress for wedding",
     category: "Personal",
     deadline: "2026-11-05",
     status: "In Progress"
   },
   {
+    id: 2,
     name: "Submit Q3 client invoices",
     category: "Work",
     deadline: "2026-09-15",
     status: "Pending"
   },
   {
+    id: 3,
     name: "Renew car registration",
     category: "Errands",
     deadline: "2026-09-10",
     status: "Pending"
   },
   {
+    id: 4,
     name: "Finish CTS Launch Camp curriculum outline",
     category: "Work",
     deadline: "2026-10-01",
     status: "In Progress"
   },
   {
+    id: 5,
     name: "Book dentist appointment",
     category: "Personal",
     deadline: "2026-08-30",
     status: "Done"
   },
   {
+    id: 6,
     name: "Prepare October class materials",
     category: "Work",
     deadline: "2026-09-25",
@@ -40,10 +49,22 @@ const tasks = [
 function updateStatus(taskId, newStatus){
     let task = tasks.find(t => t.id === taskId);
     task.status = newStatus
-    console.log(task);
+    return task;
 
 }
 
+console.log(tasks)
+
+console.log(updateStatus(3, "Done"));
+
+//give each task an id
+// function addId(){
+//     for(let i = 0; i < tasks.length; i++){
+//         console.log(i);
+//     }
+// }
+
+// console.log(addId())
 //Create status dropdown
 
 // for(let item of STATUES){
